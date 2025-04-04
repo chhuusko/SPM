98133de55af9cd9bca20777e712e9192e3dacd7d
AlexO04 <144449986+AlexO04@users.noreply.github.com>
2025-04-04 20:03:22 +0200

    Test
    Tested code and compile

---
+++ SPMGrupp6/Source/SimpleShooter/HealthPickUp.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePickUp.h"
+#include "HealthPickUp.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLESHOOTER_API AHealthPickUp : public ABasePickUp
+{
+	GENERATED_BODY()
+	
+};
