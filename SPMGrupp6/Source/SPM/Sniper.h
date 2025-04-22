9de6f6ad83c123f84fe096cc475fa87ef1faa84f
MRbubbelgum <malte.gundhus@gmail.com>
2025-04-22 14:07:37 +0200

    Temporary sniper add

---
+++ SPMGrupp6/Source/SPM/Sniper.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Gun.h"
+#include "Sniper.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPM_API ASniper : public AGun
+{
+	GENERATED_BODY()
+	
+};
