47d0f29e973a9580487eddd6b57f31e1e5607e7f
MRbubbelgum <malte.gundhus@gmail.com>
2025-04-23 10:42:01 +0200

    Sniper aim, aim & noscope accuracy
    The sniper now has the ability to zoom in, and shot accuracy changes depending on whether the player is scoped in or not.

--- SPMGrupp6/Source/SPM/Sniper.h
+++ SPMGrupp6/Source/SPM/Sniper.h
@@ -13,5 +13,26 @@ UCLASS()
 class SPM_API ASniper : public AGun
 {
 	GENERATED_BODY()
+
+private:
+	virtual void BeginPlay() override;
+	virtual void WeaponAbility() override;
+	virtual void StopWeaponAbility() override;
+	virtual bool GunTrace(FHitResult& Hit, FVector& ShotDirection, float& TraceLength) override;
+	FTimerHandle AimTimerHandle;
+	float ScopeToEyeDuration = 0.3;
+	bool bIsAimingDownSight = false;
+
+	UPROPERTY(VisibleAnywhere)
+	float OriginalPLayerFOV;
+
+	UPROPERTY(EditAnywhere)
+	float ZoomInFOV = 25;
+
+	UPROPERTY(EditAnywhere)
+	float ConeRadius = 15;
 	
+	void ZoomIn();
+	void SetCameraFOV(float amount);
+
 };
