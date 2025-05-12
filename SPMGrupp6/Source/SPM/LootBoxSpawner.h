f714fdb1cd981c70126648eda16a425c73fa512b
AlexO04 <144449986+AlexO04@users.noreply.github.com>
2025-05-12 15:36:51 +0200

    LootBoxSpawner

---
+++ SPMGrupp6/Source/SPM/LootBoxSpawner.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "LootBoxSpawner.generated.h"
+
+class ALootBox;
+
+UCLASS()
+class SPM_API ALootBoxSpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALootBoxSpawner();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+private:
+	void SpawnLootBox();
+	FTimerHandle SpawnTimerHandle;
+	UPROPERTY(EditDefaultsOnly)
+	float SpawnDelay;
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<ALootBox> LootClass;
+};
