1886b6c5db056b1f0e224d4ee8999dcbd73391ec
AlexO04 <144449986+AlexO04@users.noreply.github.com>
2025-05-12 13:10:22 +0200

    LootBox

---
+++ SPMGrupp6/Source/SPM/LootBox.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "LootBox.generated.h"
+
+class AResourcePickUp;
+
+UCLASS()
+class SPM_API ALootBox : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALootBox();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+private:
+	UPROPERTY(EditDefaultsOnly)
+	UStaticMeshComponent* Box;
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AResourcePickUp> ResourcePickUpClass;
+	UPROPERTY(EditDefaultsOnly)
+	int lootAmount;
+	void DropLoot();
+};
