7dbd186f311593cf2ad00739752855409becf55b
Jaanek Kerma <jaanek.kerma@gmail.com>
2025-05-12 18:16:40 +0200

    moved radar to a folder

--- SPMGrupp6/Source/SPM/UpgradedPistol.cpp
+++ SPMGrupp6/Source/SPM/UpgradedPistol.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "UpgradedPistol.h"
-#include "RadarComponent.h"
+#include "SPM/Minimap/RadarComponent.h"
 
 
 
