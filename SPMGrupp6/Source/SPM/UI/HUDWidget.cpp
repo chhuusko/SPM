3ade510a91401a6478a08fd68f6c1362e9edd561
Joppa0 <114592889+Joppa0@users.noreply.github.com>
2025-05-26 14:11:59 +0200

    Crosshair now gets removed on equipping sniper rifle
    - Changed from only removing crosshair when scoping, to now removing crosshair entirely when equipping the weapon.

--- SPMGrupp6/Source/SPM/UI/HUDWidget.cpp
+++ SPMGrupp6/Source/SPM/UI/HUDWidget.cpp
@@ -230,10 +230,18 @@ void UHUDWidget::UpdateEquippedWeapon(EWeaponType Weapon)
 	// Bind the function for updating the sniper scope.
 	if (Weapon == EWeaponType::SniperRifle)
 	{
-		Sniper = Cast<ASniper>(Gun);
-		if (Sniper)
+		// Hide crosshair since hipfire is inaccurate.
+		if (Crosshair->IsVisible())
 		{
-			Sniper->OnScope.AddDynamic(this, &UHUDWidget::ShowCrosshair);
+			Crosshair->SetVisibility(ESlateVisibility::Hidden);
+		}
+	}
+	else
+	{
+		// Show crosshair.
+		if (!Crosshair->IsVisible())
+		{
+			Crosshair->SetVisibility(ESlateVisibility::Visible);
 		}
 	}
 
