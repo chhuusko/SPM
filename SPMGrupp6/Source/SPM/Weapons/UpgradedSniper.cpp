671a1ceeea49bd14f5a57b40fade972fb16aee25
MRbubbelgum <malte.gundhus@gmail.com>
2025-05-26 19:11:05 +0200

    bug fix update fire rate

--- SPMGrupp6/Source/SPM/Weapons/UpgradedSniper.cpp
+++ SPMGrupp6/Source/SPM/Weapons/UpgradedSniper.cpp
@@ -17,7 +17,9 @@ void AUpgradedSniper::Fire()
 	// Checks if weapon can fire.
 	if (!bCanFire || !bIsWeaponEquipped || Cast<AShooterCharacter>(GetOwner())->IsDead()) return;
 
+	// Stops possibility to fire between shots, has slight shorter Reset to make sure timers don´t miss match.
 	bCanFire = false;
+	GetWorld()->GetTimerManager().SetTimer(BetweenShotsTimer, this, &AGun::ResetCanFire, FireRate-0.02f, false);
 
 	// Reloads automatically if bullets is when you start shooting 0.
 	if (BulletsLeft <= 0)
@@ -133,9 +135,6 @@ void AUpgradedSniper::Fire()
 		Reload();
 		return;
 	}
-	
-	// Stops possibility to fire between shots, has slight shorter Reset to make sure timers don´t miss match.
-	GetWorld()->GetTimerManager().SetTimer(BetweenShotsTimer, this, &AGun::ResetCanFire, FireRate-0.01f, false);
 	
 	OnFired.Broadcast();
 }
