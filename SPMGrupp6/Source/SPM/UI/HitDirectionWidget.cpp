e2fa10608228a73dd17ab336f60dc0fa3e268033
Joppa0 <114592889+Joppa0@users.noreply.github.com>
2025-05-29 10:10:09 +0200

    Changed hitmarker color to white

--- SPMGrupp6/Source/SPM/UI/HitDirectionWidget.cpp
+++ SPMGrupp6/Source/SPM/UI/HitDirectionWidget.cpp
@@ -5,6 +5,7 @@
 
 #include "HitIndicatorWidget.h"
 #include "Components/Image.h"
+#include "Kismet/KismetMathLibrary.h"
 #include "SPM/Characters/ShooterCharacter.h"
 
 void UHitDirectionWidget::NativeConstruct()
@@ -20,15 +21,28 @@ void UHitDirectionWidget::NativeConstruct()
 
 void UHitDirectionWidget::ShowIndicator(AActor* DamageCauser)
 {
+	if (!DamageCauser || !PlayerCharacter)
+	{
+		return;
+	}
+	
+	DamageIcon->SetVisibility(ESlateVisibility::Visible);
+	
 	FVector3d DamageLocation = DamageCauser->GetActorLocation();
 	FVector3d PlayerLocation = PlayerCharacter->GetActorLocation();
-	FVector3d DamageDirection = DamageLocation - PlayerLocation;
+	//FVector3d DamageDirection = DamageLocation - PlayerLocation;
 
-	DamageIcon->SetRenderTransformAngle(DamageDirection.Z);
+	FRotator LookRotation = UKismetMathLibrary::FindLookAtRotation(DamageLocation, PlayerLocation);
+	FRotator ControlRotation = PlayerCharacter->GetControlRotation();
+
+	float Rotation = LookRotation.Yaw - ControlRotation.Yaw;
+
+	DamageIcon->SetRenderTransformAngle(360 - Rotation);
 
 	GetWorld()->GetTimerManager().SetTimer(HideIndicatorTimer, this, &UHitDirectionWidget::HideIndicator, DisplayTime);
 }
 
 void UHitDirectionWidget::HideIndicator()
 {
+	DamageIcon->SetVisibility(ESlateVisibility::Hidden);
 }
