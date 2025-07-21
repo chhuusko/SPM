b4d57bb722475d95b6de20681eea85134fe571cf
AlexO04 <144449986+AlexO04@users.noreply.github.com>
2025-07-21 12:56:48 +0200

    instigator nullcheck

--- SPMGrupp6/Source/SPM/Drone/DroneBullet.cpp
+++ SPMGrupp6/Source/SPM/Drone/DroneBullet.cpp
@@ -39,6 +39,7 @@ void ADroneBullet::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrim
 {
 	if (AShooterCharacter* player = Cast<AShooterCharacter>(OtherActor))
 	{
+		if (!GetOwner()->GetInstigatorController()) return;
 		UGameplayStatics::ApplyDamage(player,  BaseDamage,GetOwner()->GetInstigatorController(), GetOwner(), UDamageType::StaticClass());
 	}
 	Destroy();
